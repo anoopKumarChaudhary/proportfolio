4e57ba0b485117108c5c0f293f4f5ea7a30bc9ea
unknown <64825962+D1urga@users.noreply.github.com>
2025-02-21 16:16:20 +0530

    updated

--- src/app/components/hackathons.js
+++ src/app/components/hackathons.js
@@ -48,7 +48,7 @@ const Hackathons = React.memo(() => {
             className={`${styles.masonryItem} ${styles[`item${index % 5}`]}`}
             initial={{
               opacity: 0,
-              x: Math.random() * window.innerWidth,
+              x: 50,
             }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: (index * 0.5) % 0.5 }}
